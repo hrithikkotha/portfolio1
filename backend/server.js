5d2ad2a37e1583f33f83bf943e14f4650f27c0e0
hrithikkotha <hrithikkotha@gmail.com>
2025-05-17 23:21:19 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../docs')));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { 
     useNewUrlParser: true, 
@@ -32,5 +32,5 @@ app.listen(PORT, () => {
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../docs', 'index.html'));
 });
